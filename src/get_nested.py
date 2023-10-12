a1808a6a153a35cde8f2e10ffd31034ccc6e37b7
snehal <snehal.bh2014@gmail.com>
2023-10-12 13:06:28 +0000

    python script

---
+++ src/get_nested.py
@@ -0,0 +1,19 @@
+def get_nested_value(obj, key):
+    try:
+        for k in key.split('/'):
+            obj = obj[k]
+        return obj
+    except (KeyError, TypeError):
+        return None
+
+object1 = {"a": {"b": {"c": "d"}}}
+key1 = "a/b/c"
+
+object2 = {"x": {"y": {"z": "a"}}
+key2 = "x/y/z"
+
+value1 = get_nested_value(object1, key1)
+value2 = get_nested_value(object2, key2)
+
+print("Value 1:", value1)
+print("Value 2:", value2)
